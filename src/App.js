2d956551225eaa13392ab9e630530b11f8447008
Siwema-Nikini <nikinisiwema@gmail.com>
2023-10-11 13:41:15 +0300

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      name:{
+            firstName:"siwema",
+            lastName:'Nikini'
+          },
+            age:23,
+    }
+  }
+  render(){
+    return(
+      <div>
+        <p>Hello my name is {this.state.name.firstName} and I'am {this.state.age} years old</p>
+        <button
+         onClick = {() =>{
+          this.setState({name:{firstName:'wema', lastName:'Simon'}, age:27})
+          console.log(this.state)
+        }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          change
+          </button>
+      </div>
+    )
+  }
 }
 
 export default App;
