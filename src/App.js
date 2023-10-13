5e4d309f19cb105e281372915bdd1592df0ad2bc
Siwema-Nikini <nikinisiwema@gmail.com>
2023-10-13 13:16:18 +0300

    Adding monster 1-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
 
 class App extends Component{
   constructor(){
     super();
-    this.state = {
+    this.state={
       monster1:{
-        name:'linda'
+        name:'Linda'
       },
 
       monster2:{
@@ -17,32 +16,19 @@ class App extends Component{
         name:'Jack'
       },
 
-
-      name:{
-            firstName:"siwema",
-            lastName:'Nikini'
-          },
-            age:23,
     }
   }
+
   render(){
     return(
       <div>
-        <p>MONSTER RELOADER my name is {this.state.name.firstName} and I'am {this.state.age} years old</p>
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
-        <button
-         onClick = {() =>{
-          this.setState({name:{firstName:'wema', lastName:'Simon'}, age:27})
-          console.log(this.state)
-        }}
-        >
-          change
-          </button>
+      <h1>{this.state.monster1.name}</h1>
+      <h1>{this.state.monster2.name}</h1>
+      <h1>{this.state.monster3.name}</h1>
       </div>
     )
   }
+
 }
 
 export default App;
